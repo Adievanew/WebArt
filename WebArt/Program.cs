c61a4ebcc43b2011e40c787488eaab14821a8d85
manunua <102993044+manunua@users.noreply.github.com>
2023-05-11 21:58:42 +0300

    first commit

---
+++ WebArt/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+//using OnlineShop.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+using Blazored.LocalStorage;
+using WebArt.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+builder.Services.AddAuthentication(
+    CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie();
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+//builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddAntDesign();
+builder.Services.AddBootstrapBlazor();
+builder.Services.AddCors();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<HttpContextAccessor>();
+builder.Services.AddHttpClient();
+builder.Services.AddScoped<HttpClient>();
+builder.Services.AddBlazoredLocalStorage();   // local storage
+builder.Services.AddBlazoredLocalStorage(config => config.JsonSerializerOptions.WriteIndented = true);  // local storage
+builder.Services.AddSignalR();
+builder.Services.AddScoped<IUserSessionService, UserSessionService>();
+var app = builder.Build();
+app.UseWebSockets();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseCookiePolicy();
+app.UseAuthentication();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.MapRazorPages();
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ WebArt/Services/IUserSessionService.cs
@@ -0,0 +1,20 @@
+﻿namespace WebArt.Services
+{
+    internal interface IUserSessionService
+    {
+        void SetToken(string token);
+        string GetToken();
+    }
+    public class UserSessionService : IUserSessionService
+    {
+        private string Token;
+        public void SetToken(string token)
+        {
+            Token = token;
+        }
+        public string GetToken()
+        {
+            return Token;
+        }
+    }
+}
